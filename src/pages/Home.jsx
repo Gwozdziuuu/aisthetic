a0ff26c78bb32319eaa261e1d46fb14e786e5f7f
Chrisowski Chris <krzychulcz@gmail.com>
2026-02-22 20:44:45 +0000

    File changes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import Navbar from "../components/landing/Navbar";
+import Hero from "../components/landing/Hero";
+import HowItWorks from "../components/landing/HowItWorks";
+import Solutions from "../components/landing/Solutions";
+import Stats from "../components/landing/Stats";
+import Testimonials from "../components/landing/Testimonials";
+import Treatments from "../components/landing/Treatments";
+import CTASection from "../components/landing/CTASection";
+import ContactForm from "../components/landing/ContactForm";
+import Footer from "../components/landing/Footer";
+
+export default function Home() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#080808", color: "#fff" }}>
+      <Navbar />
+      <Hero />
+      <HowItWorks />
+      <Solutions />
+      <Stats />
+      <Testimonials />
+      <Treatments />
+      <CTASection />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
+}
